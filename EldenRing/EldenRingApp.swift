b98b82bba6a0d56c75a5acd60422dd15db5fb279
chr1s78 <ayahirano2014@126.com>
2022-04-03 12:07:00 +0800

    Initial Commit

---
+++ EldenRing/EldenRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EldenRingApp.swift
+//  EldenRing
+//
+//  Created by Chr1s on 2022/4/3.
+//
+
+import SwiftUI
+
+@main
+struct EldenRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
